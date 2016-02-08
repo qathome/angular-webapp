adf1df3151c8bda09e36a8dcc653642f8ebc6c75
tonegas <tonegas@gmail.com>
2016-02-08 23:38:09 +0100

    modifiche per il refresh token

--- static/src/config.js
+++ static/src/config.js
@@ -1,16 +1,9 @@
 
 var QATHOME_CLIENT_CONFIG = {
-    server: 'http://127.0.0.1:8888',
-    client_id: 'MELlSwOp0kG2i28QCGSKL0wyWAjph7oE3nZkDlM5',
-    client_secret: 'vDynNPUwgalBhdTUrHsOydw9bw2XeHA8VDW1nX5mLhzhkI1Kg4yjuLwl2uskmPvLzBdbTuA4AAPAcexJc9ELIQevFoMlpqUFamfQonlAh2TUykjxnHFw8JJmkmb8yDoL',
-    //token:{
-    //    scope: "groups read write",
-    //    access_token: "e1GjuCla3dmCmVrqypliP3dkAK0daN",
-    //    token_type: "Bearer",
-    //    expires_in: 36000,
-    //    refresh_token: "BFsRXvj92ETr4G06RqvRrM6PVjIQJX"
-    //},
-    username: 'dottore',
-    password: 'dottore',
-    firm_id: '37a5bfd9-db34-3a08-be51-5b67db1fcf47'
+    server: 'http://qathome-dev.herokuapp.com',
+    client_id: 'CLIENT_ID',
+    client_secret: 'CLIENT_SECRET',
+    username: 'USERNAME',
+    password: 'PASSWORD',
+    firm_id: 'FIRM_ID'
 };
