0679ded6efbf349b6f8aff97c837271792f855e3
tonegas <tonegas@gmail.com>
2016-02-08 19:14:22 +0100

    tolto robba

--- static/src/config.js
+++ static/src/config.js
@@ -3,6 +3,13 @@ var QATHOME_CLIENT_CONFIG = {
     server: 'http://qathome-dev.herokuapp.com',
     client_id: 'XTXHCin4JGc3EHCZegM0QuuWEEtdWVxWfPepZSfH',
     client_secret: 'v3TfL6FNA9R0Zx4evG3acSghp6DiqVJx364hi3DY9CyKIl7ouxYPLkxBkwazBrgFzsDj9BUZvXROO2bNI0cMlowbvUyIkeBXaHhpMIky1nv18pGwF4J7ZalDJvVNXv6K',
+    token:{
+        scope: "groups read write",
+        access_token: "e1GjuCla3dmCmVrqypliP3dkAK0daN",
+        token_type: "Bearer",
+        expires_in: 36000,
+        refresh_token: "BFsRXvj92ETr4G06RqvRrM6PVjIQJX"
+    },
     username: 'dottore',
     password: 'dottore',
     firm_id: '37a5bfd9-db34-3a08-be51-5b67db1fcf47'
